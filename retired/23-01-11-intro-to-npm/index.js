dd765798b907c86ef0e622f573d9eedd347a533f
Haritha Diraneyya <haritha.diraneyya@gmail.com>
2023-01-12 15:16:33 -0500

    after the drum kit

---
+++ retired/23-01-11-intro-to-npm/index.js
@@ -0,0 +1,9 @@
+
+const supH = require("superheroes");
+const supV = require("supervillains");
+
+let mySupH01 = supH.random();
+let mySupV01 = supV.random();
+
+console.log(mySupH01);
+console.log(mySupV01);
